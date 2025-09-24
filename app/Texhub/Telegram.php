9e2cafa0bafbd5ef9724a0334015c28cea08502f
TexusCode <texus.tj@gmail.com>
2025-09-24 22:02:29 +0500

    updated

--- app/Texhub/Telegram.php
+++ app/Texhub/Telegram.php
@@ -18,14 +18,14 @@ class Telegram extends \DefStudio\Telegraph\Handlers\WebhookHandler
 {
     public function start(): void
     {
-        // $this->chat->photo(public_path('assets/welcome.jpg'))->message("Салом " . $this->message->from()->firstName() . "! \nИн телеграм боти <b>Shifu Cargo</b> мебошад! \nБарои истифода бурдан аввал забонро интихоб кунед!\n\nЭто телеграм бот <b>Shifu Cargo!</b> \nЧтобы использовать, сначала выберите язык! ⤵️")
-        //     ->keyboard(
-        //         Keyboard::make()
-        //             ->row([
-        //                 Button::make('🇹🇯 Тоҷикӣ')->action('tj'),
-        //                 Button::make('🇷🇺 Русский')->action('ru'),
-        //             ])
-        //     )->send();
-        $this->chat->message('hello')->send();
+        $this->chat->photo(public_path('assets/welcome.jpg'))->message("Салом " . $this->message->from()->firstName() . "! \nИн телеграм боти <b>Shifu Cargo</b> мебошад! \nБарои истифода бурдан аввал забонро интихоб кунед!\n\nЭто телеграм бот <b>Shifu Cargo!</b> \nЧтобы использовать, сначала выберите язык! ⤵️")
+            ->keyboard(
+                Keyboard::make()
+                    ->row([
+                        Button::make('🇹🇯 Тоҷикӣ')->action('tj'),
+                        Button::make('🇷🇺 Русский')->action('ru'),
+                    ])
+            )->send();
+        // $this->chat->message('hello')->send();
     }
 }
