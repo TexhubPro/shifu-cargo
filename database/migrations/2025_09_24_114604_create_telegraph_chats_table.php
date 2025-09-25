2921801e00a971ec01e4c25fed3bdb58df8fe98c
TexusCode <texus.tj@gmail.com>
2025-09-26 00:19:13 +0500

    updated

--- database/migrations/2025_09_24_114604_create_telegraph_chats_table.php
+++ database/migrations/2025_09_24_114604_create_telegraph_chats_table.php
@@ -4,13 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration {
     public function up(): void
     {
         Schema::create('telegraph_chats', function (Blueprint $table) {
             $table->id();
             $table->string('chat_id');
             $table->string('name')->nullable();
+            $table->string('lang')->nullable();
 
             $table->foreignId('telegraph_bot_id')->constrained('telegraph_bots')->cascadeOnDelete();
             $table->timestamps();
