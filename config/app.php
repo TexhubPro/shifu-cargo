83e735ff603c2e3c45e9d00032475ea55ddccc82
TexusCode <texus.tj@gmail.com>
2025-10-05 11:51:26 +0500

    updated

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
     'name' => env('APP_NAME', 'Laravel'),
-    // 'asset_url' => env('ASSET_URL', '/public'),
+    'asset_url' => env('ASSET_URL', '/public'),
 
     /*
     |--------------------------------------------------------------------------
