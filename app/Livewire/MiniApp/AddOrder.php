bb6fecea2d38c3286db2d1967dfa74bb16ea6afb
Mahmudi Shodmehr <mahmudishodmehr@Mac-mini-Mahmudi.local>
2025-11-07 14:55:26 +0500

    updated

--- app/Livewire/MiniApp/AddOrder.php
+++ app/Livewire/MiniApp/AddOrder.php
@@ -11,21 +11,32 @@ class AddOrder extends Component
     public $trackcode;
     public function add()
     {
+        // Проверка на соответствие требованиям
+        if (!preg_match('/^[A-Za-z0-9]{1,20}$/', $this->trackcode)) {
+            $this->dispatch('alert', '❌ Трек-код должен содержать только латинские буквы и цифры (до 20 символов)');
+            return;
+        }
+
         $code = Trackcode::where('code', $this->trackcode)->first();
+
         if ($code) {
             $code->user_id = Auth::id();
             $code->save();
-            $this->dispatch('alert', 'Трек-код уже сушествует! Обновили информацию!');
+
+            $this->dispatch('alert', '⚠️ Трек-код уже существует, информация обновлена!');
             $this->reset('trackcode');
             return;
         }
+
         Trackcode::create([
             'code' => $this->trackcode,
             'user_id' => Auth::id(),
         ]);
+
         $this->reset('trackcode');
-        $this->dispatch('alert', 'Трек-код успешно добавлено!');
+        $this->dispatch('alert', '✅ Трек-код успешно добавлен!');
     }
+
 
     public function render()
     {
