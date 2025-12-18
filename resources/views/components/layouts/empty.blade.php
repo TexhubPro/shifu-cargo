ab15c5f788a96b91ed60f076adfb438d3c867bec
Mahmudi Shodmehr <mahmudishodmehr@MacBook-Air-Mahmudi.local>
2025-12-18 06:22:36 +0500

    updated

--- resources/views/components/layouts/empty.blade.php
+++ resources/views/components/layouts/empty.blade.php
@@ -5,8 +5,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="{{ asset('obfuscated.min.css') }}">
     <title>{{ $title ?? 'Page Title' }}</title>
+    @livewireStyles
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
     @yield('styles')
@@ -16,6 +18,7 @@
 
     {{ $slot }}
     @livewire('components.alert')
+    @livewireScripts
     @fluxScripts
 </body>
 
