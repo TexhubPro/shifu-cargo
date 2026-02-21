280a236a70ce794d614392c7b826b8146783a663
Mahmudi Shodmehr <mahmudishodmehr@MacBook-Air-Mahmudi.local>
2026-02-21 12:02:29 +0500

    updated

--- resources/views/components/layouts/empty.blade.php
+++ resources/views/components/layouts/empty.blade.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full light">
+<!DOCTYPE>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" html
+    class="{{ Route::is('cashier') ? 'light' : 'dark' }}{{ Route::is('cashier.reports') ? 'light' : 'dark' }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/svg+xml" href="{{ asset('logo/favicon_color.svg') }}">
     <link rel="stylesheet" href="{{ asset('obfuscated.min.css') }}">
     <title>{{ $title ?? 'Page Title' }}</title>
     @livewireStyles
@@ -14,11 +14,10 @@
     @yield('styles')
 </head>
 
-<body class="min-h-full bg-neutral-100 text-neutral-900 antialiased">
+<body class="bg-neutral-950">
 
     {{ $slot }}
     @livewire('components.alert')
-    @include('partials.flux-modal-fallback')
     @livewireScripts
     @fluxScripts
 </body>
